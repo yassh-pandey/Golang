1e5541e6b6007b0af1ebd6fb54ec265fbc3449da
Yash_Science <yashpandey.science@gmail.com>
2020-09-12 18:11:09 +0530

    Initial commit

---
+++ findTriplets.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// FindTriplets returns all the triplets from an array so that some of 2 equals to third element
+func FindTriplets(input []int) {
+	type Triplets struct {
+		First, Second, Third int
+	}
+	result := []Triplets{}
+	resultMap := map[Triplets]bool{}
+	inputArr := make([]int, len(input))
+	copy(inputArr, input)
+	for i, v := range inputArr {
+		if i == len(inputArr)-1 {
+			break
+		}
+		firstEle := v
+		secondEle := inputArr[i+1]
+		var diff int
+		if firstEle >= secondEle {
+			diff = firstEle - secondEle
+		} else {
+			diff = secondEle - firstEle
+		}
+		for _, thirdEle := range inputArr {
+			if thirdEle == diff {
+				set := []int{firstEle, secondEle, thirdEle}
+				sort.Ints(set)
+				trip := Triplets{set[0], set[1], set[2]}
+				if val := resultMap[trip]; val == false {
+					resultMap[trip] = true
+				}
+			}
+		}
+	}
+	for k := range resultMap {
+		result = append(result, k)
+	}
+	fmt.Println(result)
+}
+func main() {
+	FindTriplets([]int{5, 32, 1, 7, 10, 50, 19, 21, 2})
+}
